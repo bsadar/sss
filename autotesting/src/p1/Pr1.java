26924629a380be8814bd0c0f3e2920f31bb6b6b2
qaplanet <qaplanet@qaplanet-PC>
2019-04-24 16:18:23 +0530

    this is new code

---
+++ autotesting/src/p1/Pr1.java
@@ -0,0 +1,20 @@
+package p1;
+
+import p3.JNTU16;
+
+public class Pr1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JNTU16 rw1=new JNTU16();
+		       rw1.sdname="sadar";
+		       rw1.rollno=1;
+		       rw1.ok();
+		       System.out.println("hi");
+		       System.out.println(  rw1.sdname);
+		       System.out.println(rw1.rollno);
+		       System.out.println("this is method execution");
+	}
+
+}
---
+++ autotesting/src/p3/JNTU16.java
@@ -0,0 +1,14 @@
+package p3;
+
+public class JNTU16 {
+
+	public  String  sdname;
+	public  int rollno;
+	
+	 public   void ok(){
+		   System.out.println("this is method execution");
+		   
+	   }
+	
+	  
+}
